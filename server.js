d995d5702cde8269bfcf6e3e3b9018b6421dbfb5
Kane Swartz <UP941226@myport.ac.uk>
2022-02-01 15:56:44 +0000

    :art:

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.HEALTHCHECKER_PORT || 8080
+const app = express();
+const port = process.env.HEALTHCHECKER_PORT || 8080;
 
-express().get('/healthcheck', (req, res) => { res.sendStatus(200) })
-express().get('/index', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')) })
-express().listen(port, () => { console.log(`healthchecker listening on ${port}`) })
+app.get('/healthcheck', (req, res) => { res.sendStatus(200) });
+app.get('/test_page', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.listen(port, () => {
+  console.log(`healthchecker listening on ${port}`);
+});
